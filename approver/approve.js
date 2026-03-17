5b88bffb4b40d4203d209c78a364baf537b80cbc
oita-joho <oitajoho@gmail.com>
2026-03-17 16:46:21 +0900

    Update approve.js

--- approver/approve.js
+++ approver/approve.js
@@ -2,7 +2,7 @@
 // approve.js
 // 承認 + 差し戻し対応版 + PIN認証対応
 // ================================
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzwiLcSMXpXxQD3Z17X8CnipLfueqd9kHPHBPYKvowO5SxzYZStxCtI0qhh-mfEFO1ndA/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxAFwRbhcNFfd2p5PmrzyGis7cS0p90Z0UMsHD0gCf31ZP945ZjQuyiC-22SlXx4_QX/exec";
 
 function $(id){ return document.getElementById(id); }
 
